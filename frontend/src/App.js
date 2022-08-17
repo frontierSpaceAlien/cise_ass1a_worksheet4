3bae12f63dfc927eb004bc9e2792c363492d99e3
frontierSpaceAlien <yxx6616@autuni.ac.nz>
2022-08-17 15:28:42 +1200

    updated files

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,13 +2,14 @@ import React from "react";
 import {
   Route,
   NavLink,
-  BrowserRouter as Router
+  BrowserRouter as Router,
+  Routes
 } from "react-router-dom";
 
-//import Home from "./pages/Home";
-//import SEPractice from "./pages/SE-Practice";
-//import SubmitArticle from "./pages/Submit-Article"; 
-//import NotFoundPage from "./pages/404";
+import Home from "./pages/Home";
+import SEPractice from "./pages/SE-Practice";
+import SubmitArticle from "./pages/Submit-Article"; 
+import NotFoundPage from "./pages/404";
 
 const App = () =>  {
     return (
@@ -16,13 +17,18 @@ const App = () =>  {
           <div>
             <h1>Software Practice Empirical Evidence Database (SPEED)</h1>
               <ul className="header">
-                  <li><a href = "/">Home</a></li>
-                  <li><a href = "/SEPractice">Select the Practice</a></li>
-                  <li><a href = "/SubmitArticle">Submit an Article</a></li>
+                <li><NavLink exact to = "/">Home</NavLink></li>
+                <li><NavLink to = "/SEPractice">Select the Practice</NavLink></li>
+                <li><NavLink to = "/SubmitArticle">Submit an Article</NavLink></li>
               </ul>
-            <div className="content">
-              <></>
-            </div>
+              <div className="content">
+                <Routes>
+                  <Route exact path="/" element={<Home />} />
+                  <Route path="/SEPractice" element={<SEPractice />} />
+                  <Route path="/SubmitArticle" element={<SubmitArticle />} />
+                  <Route path="*" element={<NotFoundPage/>} />
+                </Routes>
+              </div>
           </div>
         </Router>
     );
