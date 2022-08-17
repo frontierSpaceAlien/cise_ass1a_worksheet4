756daafedd6afa084c72110282146fcc243597e3
frontierSpaceAlien <yxx6616@autuni.ac.nz>
2022-08-17 16:31:30 +1200

    updated seper app

--- frontend/src/pages/Submit-Article.js
+++ frontend/src/pages/Submit-Article.js
@@ -1,4 +1,5 @@
 import React from "react";
+import SubmissionForm from "../components/SubmissionForm";
  
 const SubmitArticle = () =>  {
      return (
@@ -6,7 +7,7 @@ const SubmitArticle = () =>  {
         <h2>Submit Article</h2>
             <p>This will be a form to allow submitters to submit an article for possible inclusion in our SEED repo.</p>
             <p>Also the display table of evidence levels for different claims will show here for the SE practice selected</p>
-            <p>Also the display table of evidence levels for different claims will show here for the SE practice selected</p>
+            <SubmissionForm/>
         </div>
     );
 }
